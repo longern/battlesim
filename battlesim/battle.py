bbb010a7dae3883c6a9602ab27376c0f1d28419f
Long <longsiyu@megvii.com>
2021-03-18 11:51:40 +0800

    Add Game class

--- battlesim/battle.py
+++ battlesim/battle.py
@@ -29,9 +29,19 @@ class Player:
     def first_minion(self):
         return next(iter(self.minions), None)
 
+    @property
+    def has_attackable_minions(self):
+        return any(minion.attack > 0 for minion in self.minions)
 
-def has_attackable_minions(player: Player):
-    return any(minion.attack > 0 for minion in player.minions)
+
+class Game:
+    def __init__(self):
+        self.players: Player = []
+
+    @property
+    def minions(self):
+        for player in self.players:
+            yield from player.minions
 
 
 def deal_damage(amount: int, card, source=None):
@@ -59,8 +69,8 @@ def pick_attacked_target(minions):
     return random.choice(minions)
 
 
-def check_death(players: List[Player]):
-    for player in players:
+def check_death(game: Game):
+    for player in game.players:
         for minion in player.minions[:]:
             if minion.health <= 0 or minion.poisoned:
                 minion_index = player.minions.index(minion)
@@ -72,26 +82,28 @@ def check_death(players: List[Player]):
                         player.active_minion = player.minions[minion_index]
 
 
-def battle(players: List[Player]):
-    for player in players:
+def battle(game: Game):
+    for player in game.players:
         for minion in player.minions:
             # minion.start_of_combat()
             pass
 
-    for player in players:
+    for player in game.players:
         player.active_minion = next(iter(player.minions), None)
 
-    current_player_iter = cycle(players)
-    if len(players[1].minions) > len(players[0].minions) + random.uniform(-0.1, 0.1):
+    current_player_iter = cycle(game.players)
+    if len(game.players[1].minions) > len(game.players[0].minions) + random.uniform(
+        -0.1, 0.1
+    ):
         next(current_player_iter)
 
-    while any(has_attackable_minions(player) for player in players):
+    while any(player.has_attackable_minions for player in game.players):
         current_player = next(current_player_iter)
-        opponent = next(filter(lambda player: player != current_player, players))
+        opponent = next(filter(lambda player: player != current_player, game.players))
         attack(current_player.active_minion, pick_attacked_target(opponent.minions))
-        check_death(players)
+        check_death(game)
 
-        if any(not player.minions for player in players):
+        if any(not player.minions for player in game.players):
             break
 
         next_active_minion_index = (
@@ -101,11 +113,11 @@ def battle(players: List[Player]):
             next_active_minion_index = 0
         current_player.active_minion = current_player.minions[next_active_minion_index]
 
-    return bool(players[0].minions) - bool(players[1].minions)
+    return bool(game.players[0].minions) - bool(game.players[1].minions)
 
 
-def parse_battlefield(*player_minions_stats):
-    players = []
+def parse_battlefield(player_minions_stats) -> Game:
+    game = Game()
     for minions_stats in player_minions_stats:
         player = Player()
         for attack, health, *args in minions_stats:
@@ -113,16 +125,15 @@ def parse_battlefield(*player_minions_stats):
             minion.attack = attack
             minion.health = health
             for arg in filter(lambda arg: issubclass(arg, Keyword), args):
-                    setattr(minion, arg.as_attribute(), True)
+                setattr(minion, arg.as_attribute(), True)
             player.minions.append(minion)
-        players.append(player)
+        game.players.append(player)
 
-    return players
+    return game
 
 
-battlefield_data = [(1, 1, DivineShield)], [(1, 1)]
-battlefield = parse_battlefield(*battlefield_data)
+battlefield = parse_battlefield(battlefield_data)
 from collections import Counter
 
-c = Counter(battle(parse_battlefield(*battlefield_data)) for _ in range(10000))
+c = Counter(battle(parse_battlefield(battlefield_data)) for _ in range(10000))
 print(c)
