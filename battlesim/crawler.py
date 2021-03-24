2e6f73d6b8b242a4e3b6341883b5c62209bc5d7c
longern <i@longern.com>
2021-03-24 23:40:33 +0800

    Crawl child cards

--- battlesim/crawler.py
+++ battlesim/crawler.py
@@ -6,17 +6,29 @@ import os
 
 
 def crawl_cards_data() -> dict:
+    file_directory = pathlib.Path(__file__).parent
+    if not os.path.exists(file_directory / "downloads"):
+        os.mkdir(file_directory / "downloads")
+
     response = requests.get(
         "https://api.blizzard.com/hearthstone/cards?gameMode=battlegrounds&locale=en_US&pageSize=2000",
         headers={"Authorization": "Bearer KR0TZFpUeCM5q4QdrFqKfgdRUk5Wgayl4M"},
     )
 
-    file_directory = pathlib.Path(__file__).parent
-    if not os.path.exists(file_directory / "downloads"):
-        os.mkdir(file_directory / "downloads")
+    cards_data: list = response.json()["cards"]
+
+    child_card_ids = []
+    for card_data in cards_data:
+        child_card_ids.extend(card_data.get("childIds", []))
+    ids_param = "%2C".join(str(card_id) for card_id in child_card_ids)
+    child_card_response = requests.get(
+        f"https://api.blizzard.com/hearthstone/cards?ids={ids_param}&locale=en_US",
+        headers={"Authorization": "Bearer KR0TZFpUeCM5q4QdrFqKfgdRUk5Wgayl4M"},
+    )
+    cards_data.extend(child_card_response.json()["cards"])
 
     with open(file_directory / "downloads/cards.json", "w") as cards_data_file:
-        json.dump(response.json()["cards"], cards_data_file, indent=2)
+        json.dump(cards_data, cards_data_file, indent=2)
 
 
 if __name__ == "__main__":
