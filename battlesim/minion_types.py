eed75b7469a7e472def9b07f27782d6bbf3a87c4
longern <i@longern.com>
2021-03-24 09:08:02 +0800

    Add minion types

---
+++ battlesim/minion_types.py
@@ -0,0 +1,34 @@
+class MinionType:
+    pass
+
+
+class Murloc(MinionType):
+    minion_type_id = 14
+
+
+class Demon(MinionType):
+    minion_type_id = 15
+
+
+class Mech(MinionType):
+    minion_type_id = 17
+
+
+class Elemental(MinionType):
+    minion_type_id = 18
+
+
+class Beast(MinionType):
+    minion_type_id = 20
+
+
+class Pirate(MinionType):
+    minion_type_id = 23
+
+
+class Dragon(MinionType):
+    minion_type_id = 24
+
+
+class All(MinionType):
+    minion_type_id = 26
