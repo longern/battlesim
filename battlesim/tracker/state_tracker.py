b4d2c296007c4ba15b4570d12680582562853e89
longern <i@longern.com>
2021-08-14 17:01:40 +0800

    Remove unused code

--- battlesim/tracker/state_tracker.py
+++ battlesim/tracker/state_tracker.py
@@ -1,28 +1,8 @@
-import logging
-from collections import Counter
-from copy import deepcopy
-
-from hearthstone import enums
-from hearthstone.cardxml import load
-
 from battlesim.simulator.state import GameState, g
 from battlesim.tracker.exporter import GameStateExporter
 
-from ..simulator.battle import battle
 from .logfile import LogStream, get_log_path
 from .parser import BattlegroundParser
-
-db, _ = load(locale="zhCN")
-logging.getLogger().setLevel(logging.ERROR)
-
-
-def card_repr(card) -> str:
-    return "%s%d/%d" % (
-        db[card.card_id].name,
-        getattr(card, "atk", 0),
-        card.health - getattr(card, "damage", 0),
-    )
-
 
 def combat_callback(parser: BattlegroundParser):
     exporter = GameStateExporter(parser.games[-1])
